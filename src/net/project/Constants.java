e779cf3ee590a79469afb811182e8b0d41161f63
Justinn <mintzer.justin@gmail.com>
2016-11-02 22:16:55 -0400

    not found exception added

--- src/net/project/Constants.java
+++ src/net/project/Constants.java
@@ -13,13 +13,14 @@ package net.project;
  */
 public class Constants {
 
-	public static final String API_KEY = "XX X i keep uploading with my key :/";
+	public static final String API_KEY = "X X X ";
 	
 	public static final int GET_SUMMONER_BY_NAME = 0;
 	public static final int GET_STATS_SUMMARY = 1;
 	
 	public static final int BAD_REQUEST = 400;
 	public static final int UNAUTHORIZED = 401;
+	public static final int NOT_FOUND = 403;
 	public static final int STATS_DATA_NOT_FOUND = 404;
 	public static final int RATE_LIMIT_EXCEEDED = 429;
 	public static final int INTERNAL_SERVER_ERROR = 500;
